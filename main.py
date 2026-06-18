6d743be47c48c2b07be596458e23b860542f3df0
Vuwori <veeymv@gmail.com>
2026-06-18 23:38:29 +0100

    fixing consistency of variables and adding if negative numbers exist.

--- main.py
+++ main.py
@@ -3,7 +3,18 @@
 import math
 
 # asks for amount of assessments
-assessment_number = int(input("how many assessments are in this module? : "))
+while True:
+    try:
+        assessment_number = int(input("How many assessments are in this module? : "))
+
+        # make sure number is not negative
+        if assessment_number <= 0:
+            print("Please enter a positive number.")
+        else:
+            break
+
+    except ValueError:
+        print("Invalid input. Please enter a whole number.")
 
 # if assement is only 1 return the exact amount
 if assessment_number == 1:
@@ -16,7 +27,16 @@ elif assessment_number > 1:
     total = 0
 
     for i in range(1, assessment_number + 1):
-        grade = int(input(f"what is your grade for assessment {i} ? : "))
+        while True:
+            try:
+                grade = float(input(f"What is your grade for assessment {i}? : "))
+
+                if grade < 0:
+                    print("Grade cannot be negative.")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
         total += grade
 
